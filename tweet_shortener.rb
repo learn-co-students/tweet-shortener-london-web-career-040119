f8f34f03b32e7bd6235f29ed45a5671cba572d31
Francisco Costa <francisco@fcosta.pt>
2019-03-19 11:39:49 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+require 'pry'
+
+def dictionary
+  dictionary = {
+    "too" => "2",
+    "to" => "2",
+    "two" =>"2",
+    "four" => "4",
+    "for" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+
+def word_substituter(tweet)
+
+  shortened_tweet = []
+
+  tweet.split().collect do |word|
+    words_to_change = dictionary.keys
+    if words_to_change.include?(word.downcase)
+      word = dictionary[word.downcase]
+      shortened_tweet << word
+    else
+      shortened_tweet<< word
+    end
+  end
+  shortened_tweet.join(" ")
+end
